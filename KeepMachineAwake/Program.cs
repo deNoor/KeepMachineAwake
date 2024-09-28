ad94d6edb2c89122bdc027e2a29aa4f96313a9e9
deNoor <denoorx@gmail.com>
2024-09-28 19:11:59 +0300

    [Init]: create console version.

---
+++ KeepMachineAwake/Program.cs
@@ -0,0 +1,25 @@
+﻿using Vanara.PInvoke;
+
+namespace KeepMachineAwake;
+
+internal class Program
+{
+    private static void Main()
+    {
+        do
+        {
+            var result = (uint) Kernel32.SetThreadExecutionState(
+                Kernel32.EXECUTION_STATE.ES_CONTINUOUS
+                | Kernel32.EXECUTION_STATE.ES_SYSTEM_REQUIRED
+                | Kernel32.EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+            if (result > 0)
+            {
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss} continuous system and display on.");
+                break;
+            }
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} failed to {nameof(Kernel32.SetThreadExecutionState)}.");
+            Thread.Sleep(TimeSpan.FromMinutes(1));
+        } while (true);
+        Thread.Sleep(Timeout.InfiniteTimeSpan);
+    }
+}
